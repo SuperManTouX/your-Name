fa9e9560a7bc513f4fc934f10640f9bd941e6471
DouHua <798521692@qq.com>
2022-06-09 23:08:43 +0800

    final cut

--- js/index.js
+++ js/index.js
@@ -1,31 +1,16 @@
-$(function () {
-    // 文档加载渐变
-    $('html').hide()
-    $('html').fadeIn(1000)
-    $('header').hide()
-    setInterval(function () {
-        $('header').slideDown(600)
-    }, 400)
-    // 滚动渐变动画
-    // $(window).scroll(function () {
-
-    //     var boxTop = $('section').eq(3).offset().top-500
-    //     if ($(document).scrollTop() >= boxTop) {
-    //         $('section').eq(3).animate({
-    //             opacity: 1
-    //         })
-    //     } else {
-    //         $('section').eq(3).animate({
-    //             opacity: 0
-    //         })
-    //     }
-    //     console.log($(document).scrollTop());
-    // })
-    // console.log($('section').eq(3).offset().top);
+$("html").animate({
+  opacity:'1',
+},800);
+$("header").hide();
+$("header").slideDown(600);
 
 
+  $("section div").smoove({
+    opacity : "1",
+    moveY: "100px",
+  });
+  $("story-main").smoove({
+    opacity : "0",
 
+  });
 
-
-
-})
